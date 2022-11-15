c14795d2cc8229e5826c50b3ffba75d7dca16833
a8hokkumar <ashokkumar.palanisamy@gyanmatrix.com>
2022-11-15 11:07:16 +0530

    Updated config

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axio = axios.create({
-  baseURL: "https://mern-acadamy-app.herokuapp.com",
+  baseURL: "https://learning-mern-academy.herokuapp.com",
   //http://localhost:8001
   headers: { 
   "X-Custom-Header": "foobar",
